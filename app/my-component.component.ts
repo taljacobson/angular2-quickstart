8754bcb5b28d4fdc651e5e5afda9b8e73576029f
taljacobson <tal1989@gmail.com>
2016-07-23 23:13:42 +0300

    expand first component

--- app/my-component.component.ts
+++ app/my-component.component.ts
@@ -2,11 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-component',
-  template: `<h1>{{name}}</h1>`
+  template: `
+  <h1>hello : {{name}}</h1>
+  <img [src] = "imageUrl"/>
+  <br/>
+  <button (click) = "changeMessage()" >button</button>
+  <h2>{{clickMessage}}</h2>
+  <input [(ngModel)] = "name" (ngModelChange) = "setUpperCase($event)"/>
+  `
 })
 export class MyComponent {
+  clickMessage;
   name: string;
-  constructor(){
+  imageUrl: string;
+  isActive: boolean;
+  constructor() {
     this.name = 'tal';
+    this.imageUrl = 'http://lorempixel.com/400/200';
+    this.isActive = true;
   }
+
+  changeMessage(){
+    this.clickMessage = 'hellow world';
+  }
+  setUpperCase(e){
+    this.name = e.toUpperCase()
+  }
+
 }
