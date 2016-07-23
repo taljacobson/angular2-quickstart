6295f342f71ce210bbba01520fa6df7dd951639b
taljacobson <tal1989@gmail.com>
2016-07-23 17:16:01 +0300

    my component inject

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { MyComponent } from './my-component.component'
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    directives: [MyComponent],
+    template: `
+      <h1>My First Angular 2 App</h1>
+      <my-component></my-component>
+      `
 })
 export class AppComponent { }
